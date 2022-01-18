7d91d9506a235177047d0d3827722a8f5fc3603b
Kevin Bamwesa Ishara <kevi3758@elev.techcollege.dk>
2022-01-18 15:28:01 +0100

    Add project files.

---
+++ BiakApp/BiakApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BiakApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StartUp();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
